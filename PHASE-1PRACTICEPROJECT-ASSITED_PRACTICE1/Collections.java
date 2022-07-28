91cf9fcd523d94f971ec7f3f50066cbdd0de3011
MadhuvarshiniS <109650018+MadhuvarshiniS@users.noreply.github.com>
2022-07-28 14:19:09 +0530

    Create Collections.java

---
+++ PHASE-1PRACTICEPROJECT-ASSITED_PRACTICE1/Collections.java
@@ -0,0 +1,15 @@
+package Collections;
+import java.util.*;  
+class collections{ 
+	public static void main(String args[]){  
+		ArrayList<String> list=new ArrayList<String>(); 
+        list.add("Ravi");  
+        list.add("Vijay");  
+        list.add("Ravi");  
+        list.add("Ajay");  
+        Iterator itr=list.iterator();  
+        while(itr.hasNext()){  
+        	System.out.println(itr.next());  
+        }
+	}
+}
