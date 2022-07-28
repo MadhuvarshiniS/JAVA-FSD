90710dd6b556ec645fafd8a20cbefeb735c62bce
MadhuvarshiniS <109650018+MadhuvarshiniS@users.noreply.github.com>
2022-07-28 14:20:11 +0530

    Create calling_method.java

---
+++ PHASE-1PRACTICEPROJECT-ASSITED_PRACTICE1/calling_method.java
@@ -0,0 +1,8 @@
+package calling_methos;
+public class calling {
+	public static void main(String[] args) {
+		int a;  
+		a=Math.min(23,98);  
+        System.out.println("Minimum number is: " + a);
+	}
+}
